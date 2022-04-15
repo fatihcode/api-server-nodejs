4ba4116ffa5a6e1a88834093c6af8989389b0e62
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-04-15 03:28:09 +0300

    bugfix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-   res.header('access-control-allow-origin', 'https://fatihqaz.github.io/');
+   res.header('access-control-allow-origin', 'https://fatihqaz.github.io');
    next();
    });
 
